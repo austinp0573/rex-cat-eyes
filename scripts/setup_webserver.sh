74075cd2a336e4d86c112e04c9388522c5c26c07
Austin P <139178212+austinp0573@users.noreply.github.com>
2026-05-19 15:02:44 -0500

    adjustments to setup_webserver.sh

--- scripts/setup_webserver.sh
+++ scripts/setup_webserver.sh
@@ -3,22 +3,17 @@
 # 1. Clear memory caches to free up every possible drop of RAM
 sync; echo 3 > /proc/sys/vm/drop_caches
 
-# Permanent Swap Setup
-dd if=/dev/zero of=/swapfile bs=1M count=128
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-
-# Ensure swap persists after reboot
-echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
-
-# Tell Alpine to only swap as a last resort
-sysctl vm.swappiness=1
-echo "vm.swappiness=1" >> /etc/sysctl.conf
+# removed swap setup portion
+# use setup-swap.sh for that
 
 # 3. Update repositories and install nginx
 apk update
-apk add nginx
+apk add nginx tzdata
+
+# Copy timezone data and immediately purge package to reclaim space
+cp /usr/share/zoneinfo/America/Chicago /etc/localtime
+echo "America/Chicago" > /etc/timezone
+apk del tzdata
 
 # 5. Create the web root directory
 mkdir -p /var/www/cat-eyes
